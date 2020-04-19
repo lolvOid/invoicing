b2c5f816f8f336dcabc2338ca94cc5581c4ac22e
lolvOid <linhtet1998itachi@gmail.com>
2020-04-19 06:47:43 +0630

    added number traslator

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,20 @@
 
 $(document).ready(function() {
 
+
+    
+
     $("#save").click(function() {
         $("#invoice-form").submit();
     
     });
+
+
+    $("#addrow").click(function(){
+
+    });
+
+
 
  });
 
---
+++ js/numbertranslator.js
@@ -0,0 +1,24 @@
+var data = null;
+var number = $("#totalamount").text();
+var inwords = $("#inwords");
+console.log(number);
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+        
+        var responseText = JSON.parse(this.responseText);
+        
+        var data = responseText.contents;
+        inwords.attr("value",data["translated"]);
+        inwords.attr("readonly","true");
+	}
+});
+
+xhr.open("GET", "https://numbers-spell.p.rapidapi.com/numbers?text="+number);
+xhr.setRequestHeader("x-rapidapi-host", "numbers-spell.p.rapidapi.com");
+xhr.setRequestHeader("x-rapidapi-key", "bae8ac0f06mshb1c40c0f1358dabp196ffdjsn235588ef714c");
+xhr.setRequestHeader("x-funtranslations-api-secret", "");
+
+xhr.send(data);
