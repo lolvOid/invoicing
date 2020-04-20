235fcc4869c6af82f4aa446b22eb2d1b5ba4ff45
lolvOid <linhtet1998itachi@gmail.com>
2020-04-21 02:05:25 +0630

    updated and resizing document

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,55 @@
+var page = $("#invoiceholder");
+
+var scale = Math.min(100/page.width(),100/page.height());
+// var defaultzoom = 0.4;
+
 
 $(document).ready(function() {
 
+   
+    zoom(defaultzoom);
     update();
 
-    $("#save").click(function() {
-        $("#invoice-form").submit();
-    
+    var _save =document.querySelectorAll("#save");
+
+    _save.forEach(element => {
+        element.addEventListener('click',function(e){           
+            $("#invoice-form").sin
+        });
     });
-
-
+    
     $("#addrow").click(function(){
 
     });
 
-   
-
  });
 
 
+function zoom(value){
+   
+        page.css("transform","scale("+ value+")");
+        $("#zoomin").click(function(){
+            
+            if(value<0.8){
+                value +=scale;              
+                page.css("transform","scale("+ value+")");
+                console.log(value);
+            }
+        });
+   
+        $("#zoomout").click(function(){      
+            if(value >=0.45){
+                value -=scale;      
+                page.css("transform","scale("+ value+")");      
+                console.log(value);
+            }
+        });
+    
+}
+
 
 function update(){
-
+    
     updatePrice();
     updateName();
     requestAnimationFrame(update);
@@ -68,3 +97,53 @@ function updatePrice(){
 
  }
 
+ const slider = document.querySelector('.wrapper');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
+//  (function(){
+//     var curYPos, curXPos, curDown;
+  
+//     window.addEventListener('mousemove', function(e){ 
+//       if(curDown){
+//         window.scrollBy(curXPos - e.pageX, curYPos - e.pageY);
+//       }
+//     });
+  
+//     window.addEventListener('mousedown', function(e){ 
+//       curYPos = e.pageY; 
+//       curXPos = e.pageX; 
+//       curDown = true; 
+//     });
+  
+//     window.addEventListener('mouseup', function(e){ 
+//       curDown = false; 
+//     });
+//   })()
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
--- js/pdfcovert.js
+++ js/pdfcovert.js
@@ -4,27 +4,33 @@ var node = $("#invoiceholder");
 
 
 $(document).ready(function() {
+    var _export =document.querySelectorAll("#export");
 
-    $("#export").click(function(){
-       genPDF();
-       $("#convertToText").show();
-    })
+    _export.forEach(element => {
+        element.addEventListener('click',function(e){           
+                genPDF();
+        });
+    });
+    
+
 });
 
 function genPDF(){
+    var page = $("#invoiceholder");
+    // page.css("transform","scale("+ 0.8+")");
     $("#convertToText").hide();
     var HTML_Width = node.width();
     var HTML_Height = node.height();
     var top_left_margin = 15;
     var PDF_Width = HTML_Width + (top_left_margin * 2);
-    var PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+    var PDF_Height = (PDF_Width * 1.8) + (top_left_margin * 2);
     var canvas_image_width = HTML_Width;
     var canvas_image_height = HTML_Height;
 
     var totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
     html2canvas(node[0]).then(function (canvas) {
         var imgData = canvas.toDataURL("images/jpeg", 1.0);
-        var pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+        var pdf = new jsPDF('p', 'pt', "[PDF_Width, PDF_Height]");
         pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
         for (var i = 1; i <= totalPDFPages; i++) { 
             pdf.addPage(PDF_Width, PDF_Height);
